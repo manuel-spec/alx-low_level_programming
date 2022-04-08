4d9cf3c36f0cbd111419de667a80e63c7fe43056
manuel-spec <amaniase084@gmail.com>
2022-04-08 14:14:48 +0300

    day_2_2

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -10,13 +10,13 @@ int main(void)
 	int n = '0';
 	int a_to_f = 'a';
 
-	while (n <= '9') 
+	while (n <= '9') /*print 0-9*/
 	{
 		putchar(n);
 		n++;
 	}
 
-	while (a_to_f <= 'f') 
+	while (a_to_f <= 'f') /*print a-f*/
 	{
 		putchar(a_to_f);
 		a_to_f++;
